0173756d6610b118676f2399587c840af5c96257
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2025-10-16 22:56:49 -0300

    Refactor ctb0039.R: remove unused package loading, enhance comments for clarity, and improve variable naming in data processing

--- ctb0039/ctb0039.R
+++ ctb0039/ctb0039.R
@@ -12,9 +12,6 @@ if (!require("sf")) {
 if (!require("openxlsx")) {
   install.packages("openxlsx")
 }
-if (!require("mapview")) {
-  install.packages("mapview")
-}
 
 # Source helper functions
 source("./helper.R")
@@ -24,6 +21,9 @@ source("./helper.R")
 # As vertentes características e os sistemas pedológicos como instrumentos de análise para a
 # identificação das fragilidades e potencialidades ambientais na Bacia Hidrográfica do Paraná 3
 file_path <- path.expand("~/ownCloud/febr-repo/processamento/ctb0039/2021-08-09-ctb0039.xlsx")
+
+# The study includes auger holes and soil profiles. However, only the soil profiles have been
+# included in this dataset so far
 
 # citation #########################################################################################
 ctb0039_citation <- openxlsx::read.xlsx(file_path, sheet = "identificacao")
@@ -52,10 +52,12 @@ str(ctb0039_event)
 # Process fields
 # observacao_id
 ctb0039_event[, observacao_id := as.character(observacao_id)]
+# check for duplicated ids
 ctb0039_event[, .N, by = observacao_id][N > 1]
 
-# data_ano
 # observacao_data
+# data_ano
+# The date is in Excel format (days since 1899-12-30)
 data.table::setnames(ctb0039_event, old = "observacao_data", new = "data_ano")
 t0 <- "1899-12-30"
 ctb0039_event[, data_ano := as.Date(data_ano, origin = t0, format = "%Y-%m-%d")]
@@ -63,7 +65,7 @@ ctb0039_event[, data_ano := as.integer(format(data_ano, "%Y"))]
 ctb0039_event[, .N, by = data_ano]
 
 # ano_fonte
-# A data de coleta no campo está especificada no documento de origem dos dados
+# The date of collection in the field is specified in the original data document
 ctb0039_event[!is.na(data_ano), ano_fonte := "original"]
 ctb0039_event[, .N, by = ano_fonte]
 
@@ -109,26 +111,35 @@ ctb0039_event[, .N, by = municipio_id]
 ctb0039_event[, amostra_area := as.numeric(amostra_area)]
 summary(ctb0039_event[, amostra_area])
 
+# taxon_sibcs_2013
 # taxon_sibcs
-# taxon_sibcs_2013 -> taxon_sibcs
 data.table::setnames(ctb0039_event, old = "taxon_sibcs_2013", new = "taxon_sibcs")
 ctb0039_event[, taxon_sibcs := as.character(taxon_sibcs)]
 ctb0039_event[, .N, by = taxon_sibcs]
 
 # taxon_st
-# Classificação do solo segundo o Soil Taxonomy não está disponível neste dataset.
+# The soil classification according to Soil Taxonomy is not available in this dataset.
 ctb0039_event[, taxon_st := NA_character_]
 
-# Pedregosidade (superficie)
-# Não tenho acesso a este trabalho após a inserção das variaveis pedregosidade e rochosidade
-# Logo, irei colocar NA_character_ para as variaveis.
-
+# pedregosidade
+# This variable is not available in this dataset. However, for two soil classes (Latossolo and
+# Nitossolo), we can assume no stone content.
 ctb0039_event[, pedregosidade := NA_character_]
+ctb0039_event[
+  grepl("Latossolo|Nitossolo", taxon_sibcs),
+  pedregosidade := "ausente"
+]
+ctb0039_event[, .N, by = pedregosidade]
 
-# Rochosidade (superficie)
-# review the work at another time
-
+# rochosidade
+# This variable is not available in this dataset. However, for two soil classes (Latossolo and
+# Nitossolo), we can assume no rock content.
 ctb0039_event[, rochosidade := NA_character_]
+ctb0039_event[
+  grepl("Latossolo|Nitossolo", taxon_sibcs),
+  rochosidade := "ausente"
+]
+ctb0039_event[, .N, by = rochosidade]
 
 str(ctb0039_event)
 
@@ -145,8 +156,8 @@ ctb0039_layer[, .N, by = observacao_id]
 # camada_nome
 ctb0039_layer[, camada_nome := as.character(camada_nome)]
 ctb0039_layer[, .N, by = camada_nome]
-ctb0039_layer[, camada_nome := gsub("B nit 1", "Bt", camada_nome)]
-ctb0039_layer[, camada_nome := gsub("B nit 2", "Bt", camada_nome)]
+ctb0039_layer[, camada_nome := gsub("B nit 1", "Bt1", camada_nome)]
+ctb0039_layer[, camada_nome := gsub("B nit 2", "Bt2", camada_nome)]
 ctb0039_layer[, camada_nome := gsub("B nitico", "Bt", camada_nome)]
 ctb0039_layer[, .N, by = camada_nome]
 
@@ -171,50 +182,68 @@ ctb0039_layer[, camada_id := 1:.N, by = observacao_id]
 ctb0039_layer[, .N, by = camada_id]
 
 # terrafina
-# terrafina is missing. We assume it is 1000 g/kg
-ctb0039_layer[, terrafina := 1000]
+# The fine earth fraction is not available in this dataset. However, for some soil classes
+# (Latossolo and Nitossolo), we can assume no gravel content, i.e., terrafina = 1000 g/kg. We will
+# set this after we merge events and layers.
+ctb0039_layer[, terrafina := NA_real_]
 
-# argila
 # argila_naoh_pipeta
+# argila
 data.table::setnames(ctb0039_layer, old = "argila_naoh_pipeta", new = "argila")
 ctb0039_layer[, argila := as.numeric(argila)]
 summary(ctb0039_layer[, argila])
+check_empty_layer(ctb0039_layer, "argila")
 
-# silte
 # silte_naoh_calc
+# silte
 data.table::setnames(ctb0039_layer, old = "silte_naoh_calc", new = "silte")
 ctb0039_layer[, silte := as.numeric(silte)]
 summary(ctb0039_layer[, silte])
+check_empty_layer(ctb0039_layer, "silte")
 
-# areia
 # areia_naoh_peneira
+# areia
 data.table::setnames(ctb0039_layer, old = "areia_naoh_peneira", new = "areia")
 ctb0039_layer[, areia := as.numeric(areia)]
 summary(ctb0039_layer[, areia])
+check_empty_layer(ctb0039_layer, "areia")
 
-# carbono
-# matorg_xxx_xxx_xxx (g/dm^3) * 0.58
+# Check that sand + silt + clay = 1000
+ctb0039_layer[, psd := argila + silte + areia]
+psd_limits <- c(900, 1100)
+# Check that the particle size distribution is within the limits
+ctb0039_layer[, psd_check := ifelse(psd < psd_limits[1], "below", ifelse(psd > psd_limits[2], "above", "within"))]
+ctb0039_layer[, .N, by = psd_check]
+
+# old: matorg_xxx_xxx_xxx
+# new: carbono
+# carbono = matorg_xxx_xxx_xxx (g/dm^3) * 0.58
+# Note that the original data is in g/dm³, i.e. volume basis.
 data.table::setnames(ctb0039_layer, old = "matorg_xxx_xxx_xxx", new = "carbono")
 ctb0039_layer[, carbono := as.numeric(carbono) * 0.58]
 summary(ctb0039_layer[, carbono])
+check_empty_layer(ctb0039_layer, "carbono")
 
-# ph
 # ph_cacl2_001mol10_eletrodo
+# ph
 data.table::setnames(ctb0039_layer, old = "ph_cacl2_001mol10_eletrodo", new = "ph")
 ctb0039_layer[, ph := as.numeric(ph)]
 summary(ctb0039_layer[, ph])
+check_empty_layer(ctb0039_layer, "ph")
 
-# ctc
 # ctc_soma_calc
+# ctc
 data.table::setnames(ctb0039_layer, old = "ctc_soma_calc", new = "ctc")
 ctb0039_layer[, ctc := as.numeric(ctc)]
 summary(ctb0039_layer[, ctc])
+check_empty_layer(ctb0039_layer, "ctc")
 
-# dsi
 # dsi_cilindro
+# dsi
 data.table::setnames(ctb0039_layer, old = "dsi_cilindro", new = "dsi")
 ctb0039_layer[, dsi := as.numeric(dsi)]
 summary(ctb0039_layer[, dsi])
+check_empty_layer(ctb0039_layer, "dsi")
 
 str(ctb0039_layer)
 
@@ -222,10 +251,19 @@ str(ctb0039_layer)
 # events and layers
 ctb0039 <- merge(ctb0039_event, ctb0039_layer, all = TRUE)
 ctb0039[, dataset_id := "ctb0039"]
+
+# Set terrafina = 1000 g/kg for Latossolo and Nitossolo and layer not R
+ctb0039[
+  grepl("Latossolo|Nitossolo", taxon_sibcs) & !grepl("R$", camada_nome),
+  terrafina := 1000
+]
+summary(ctb0039[, terrafina])
+check_empty_layer(ctb0039, "terrafina")
+
 # citation
 ctb0039 <- merge(ctb0039, ctb0039_citation, by = "dataset_id", all.x = TRUE)
 summary_soildata(ctb0039)
-# Layers: 35
+# Layers: 37
 # Events: 9
 # Georeferenced events: 9
 
