c5c08071254d4d9214a9a011d94eb4c35f4e3adb
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-10-25 15:33:25 -0300

    Remove randomization of R script execution order in main.R

--- _main/main.R
+++ _main/main.R
@@ -47,7 +47,6 @@ exceptions <- c(
 )
 r_scripts <- list.files(pattern = "\\.R$", recursive = TRUE)
 r_scripts <- r_scripts[!r_scripts %in% exceptions]
-r_scripts <- sample(r_scripts) # randomize order
 
 # Source each R script
 # Run each R script in a separate, clean R session to avoid variable conflicts.
